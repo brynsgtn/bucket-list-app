6406ac3e53985a1f4372eaf3b7aa6e439fc8857f
brynsgtn <bryansuguitan17@gmail.com>
2025-01-04 16:13:00 -0600

    Implemented routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+
+
 const PORT = process.env.PORT || 4000;
+import userRoutes from './routes/userRoutes.js';
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Welcome to user access app API')
 });
+
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
