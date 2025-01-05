779455d4b9637ef115f0696fd28e83e37fde7189
brynsgtn <bryansuguitan17@gmail.com>
2025-01-05 11:35:51 -0600

    Changed cookie expiration

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
         sameSite: 'strict',
-        maxAge: 30 * 24 * 60 * 60 * 1000
+        maxAge: 400 * 24 * 60 * 60 * 1000
     });
     
 };
