f673e9cb2d5c748b475aae4520929bb7613c2262
brynsgtn <bryansuguitan17@gmail.com>
2025-01-05 11:59:48 -0600

    Implemented auth user functionality

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,20 +1,33 @@
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js'
 
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
-const authUser = asyncHandler (async (req, res) => {
-    res.status(400);
-    throw new Error('Something went wrong');
-    
-    res.status(200).json({ message: 'Auth User' })
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPasswords(password))) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 
 // @desc Register user
 // route POST /api/users
 // @access Public
-const registerUser = asyncHandler (async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
 
     const userExists = await User.findOne({ email });
@@ -31,6 +44,7 @@ const registerUser = asyncHandler (async (req, res) => {
     });
 
     if (user) {
+        generateToken(res, user._id);
         res.status(201).json({
             _id: user._id,
             name: user.name,
@@ -53,14 +67,14 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @desc Get user profile
 // route GET /api/users/profile
 // @access Private
-const getUserProfile = asyncHandler (async (req, res) => {
+const getUserProfile = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get User Profile' })
 });
 
 // @desc Uodate user profile
 // route PUT /api/users/profile
 // @access Private
-const updateUserProfile = asyncHandler (async (req, res) => {
+const updateUserProfile = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Update User Profile' })
 });
 
