795b777c09d0f0005d9d37194d0991e628b359b9
brynsgtn <bryansuguitan17@gmail.com>
2025-01-05 12:42:51 -0600

    Implemented getUserProfile functionality

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -74,7 +74,15 @@ const logoutUser = asyncHandler(async (req, res) => {
 // route GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get User Profile' })
+
+    const user = {
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email,
+        isAdmin: req.user.isAdmin
+    }
+
+    res.status(200).json(user)
 });
 
 // @desc Uodate user profile
