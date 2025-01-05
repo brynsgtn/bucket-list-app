707b4865da0af0a3ff3c7a57cc3ca539e2b086fe
brynsgtn <bryansuguitan17@gmail.com>
2025-01-04 20:47:14 -0600

    Implemented notFound and errorHandler middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-
-
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
 const PORT = process.env.PORT || 4000;
 import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
+app.use('/api/users', userRoutes);
+
 app.get('/', (req, res) => {
     res.send('Welcome to user access app API')
 });
 
-app.use('/api/users', userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
