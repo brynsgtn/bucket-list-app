b0d643be4125ca91dd40e16fb7a33253d95e654b
brynsgtn <bryansuguitan17@gmail.com>
2025-01-14 22:59:14 -0600

    Implemented routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  RouterProvider, 
+  Route 
+} from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import Landing from './pages/Landing.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<Landing />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      {/* Private Routes
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/profile' element={<ProfileScreen />} />
+      </Route> */}
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
