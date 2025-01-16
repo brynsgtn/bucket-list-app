3e5dfb80d9f7b19d96536a815703df7b654d6ecb
brynsgtn <bryansuguitan17@gmail.com>
2025-01-15 20:59:25 -0600

    Configured redux setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { 
-  createBrowserRouter, 
-  createRoutesFromElements, 
-  RouterProvider, 
-  Route 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route
 } from "react-router-dom";
+import store from './store.js';
+import { Provider } from 'react-redux';
 import './index.css'
 import App from './App.jsx'
 import Landing from './pages/Landing.jsx';
@@ -28,7 +30,10 @@ const router = createBrowserRouter(
 )
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  </Provider>
+
 )
